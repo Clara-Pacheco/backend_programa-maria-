03cd07990d89f5d8409a7aea2188c34d517680d1
Clara <clarapachecodev@gmail.com>
2023-07-01 19:38:09 -0300

    Create a new route for path /mulher sending a json response containing info of a woman

---
+++ mulher.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const router = express.Router()
+const app = express()
+const porta = 3333
+
+function mostraOla(request,response) {
+  response.send("Olá, Mundo!")
+}
+
+function mostraMulher(request,response){
+  response.json({
+    nome: 'Clara Pacheco',
+    avatar: 'https://avatars.githubusercontent.com/u/93992748?v=4',
+    miniBio: 'Desenvolvedora e UX Designer'
+  })
+}
+
+function mostrarPorta(){
+  console.log("Servidor criado e rodando na porta", porta)
+}
+
+// Rotas da aplicação:
+app.use(router.get("/ola", mostraOla))
+app.use(router.get("/mulher" ,mostraMulher))
+app.listen(porta, mostrarPorta)
