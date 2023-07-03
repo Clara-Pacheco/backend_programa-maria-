603a95e4d60f49c09a5f14c3347bf4923d2ba746
Clara <clarapachecodev@gmail.com>
2023-07-03 11:14:11 -0300

    Import the database connection to server and execute it

--- mulheres.js
+++ mulheres.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const { v4: uuidv4 } = require('uuid')
+const conectaBancoDeDados = require('./bancoDeDados')
+conectaBancoDeDados()
 const app = express()
 app.use(express.json())
 const porta = 3333
