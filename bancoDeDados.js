768b46254c033e5a344b8a6724e8a16a6828b460
Clara <clarapachecodev@gmail.com>
2023-07-04 09:37:12 -0300

    Change connection string

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -4,7 +4,7 @@ async function conectaBancoDeDados(){
  try{
     console.log("A conexão com o banco de dados foi iniciada")
 
-    await mongoose.connect("mongodb+srv://clarapacheco27:w9DcQ5DpHNwukPN4@clustermulheres.p9myone.mongodb.net/?retryWrites=true&w=majority")
+    await mongoose.connect("mongodb+srv://clarapachecodev:I0Jq6bT9nZn2Kepa@clustermulheres.cocqf0h.mongodb.net/?retryWrites=true&w=majority")
 
     console.log("Conexão com banco de dados feita com sucesso")
 
